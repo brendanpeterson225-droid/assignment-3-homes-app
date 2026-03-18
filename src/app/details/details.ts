2e5c1b5b6b93aa92cc4776367fca18c05e3a92f4
brendanpeterson225-droid <brendanpeterson225@gmail.com>
2026-03-18 18:49:13 -0500

    integrated details page into the application

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing-service';
+import { HousingLocationInfo } from '../housinglocationinfo';
 
 @Component({
   selector: 'app-details',
@@ -7,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './details.css',
 })
 export class Details {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
